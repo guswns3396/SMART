cba9b00da817e95463b3cca1624fa85c7d02ebd2
Hyun-Joon Yang <yanghyun@stanford.edu>
2022-09-06 16:21:31 -0700

    Study parsing for nodes

--- app/Study.py
+++ app/Study.py
@@ -1,42 +1,69 @@
-class Level():
-    """
-    Level class that contains texts and number of branches
-    """
-    def __init__(self, lvl:int, numbranch:int, texts:list):
-        """
-        Instantiates Level class
-        :param lvl: level number
-        :param numbranch: number of branches following the level
-        :param texts: list of texts corresponding to number of
-        branches of previous level
-        """
-        assert isinstance(lvl, int)
-        assert isinstance(numbranch, int)
-        assert isinstance(texts, list)
-        self.lvl = lvl
-        self.numbranch = numbranch
-        self.texts = texts
+from uuid import uuid4
 
-class Study():
+
+class Node:
+
+    idx = 0
+    def __init__(self, txt: str):
+        assert isinstance(txt, str)
+        self.idx = Node.idx
+        self.txt = txt
+        self.left = None
+        self.right = None
+        self.count = 0
+        Node.idx += 1
+
+    def __getitem__(self, key):
+        if key == -1:
+            return self.left
+        elif key == 1:
+            return self.right
+        else:
+            raise ValueError
+
+    def set_left(self, node: 'Node'):
+        self.left = node
+
+    def set_right(self, node: 'Node'):
+        self.right = node
+
+    def print(self):
+        print(self.idx, self.txt)
+        if self.left:
+            self.left.print()
+        if self.right:
+            self.right.print()
+
+
+class Study:
     """
     Study class that contains the parameters
     and attributes for a particular study
     """
-    def __init__(self, parameters:dict):
+
+    def __init__(self, parameters: dict):
         """
         Instantiate Study class
         :param parameters: dict containing levels, text at each level,
         and number of branches at each level
         """
-        # parse dict to instantiate levels
-        lvls = [
-            Level(
-                lvl=int(lvl),
-                numbranch=int(parameters[lvl]['numbranch']),
-                texts=[parameters[lvl][scn] for scn in parameters[lvl] if scn != 'numbranch']
-            ) for lvl in parameters
-        ]
-        # verify number of branches from previous level
-        # matches number of texts at the current level
-        for i in range(1,len(lvls)):
-            assert len(lvls[i].texts) == lvls[i-1].numbranch
+        # parse dict to create study tree
+        self.root = Node(parameters['lvls'][0][0])
+        currnodes = [self.root]
+        for i in range(len(parameters['lvls'])):
+            nxtnodes = []
+            if i == 0:
+                continue
+            for node in currnodes:
+                left = Node(parameters['lvls'][i][0])
+                right = Node(parameters['lvls'][i][1])
+                node.set_left(left)
+                node.set_right(right)
+                nxtnodes.extend([left, right])
+            currnodes = nxtnodes
+
+        # probability
+        self.p = float(parameters['p'])
+
+        # generate id for the study
+        self.id = uuid4()
--- app/test_Study.py
+++ app/test_Study.py
@@ -3,19 +3,48 @@ import json
 
 import Study
 
+
 class TestStudy(unittest.TestCase):
     def test_instantiation(self):
         parameters = {
-            0: {
-                'scn1': '0-txt1',
-                'numbranch': 2
-            },
-            1: {
-                'scn1': '1-txt1',
-                'scn2': '1-txt2',
-                'numbranch': 0
-            }
+            'lvls': [
+                [
+                    '0-txt0'
+                ],
+                [
+                    '1-txt0',
+                    '1-txt1',
+                ],
+                [
+                    '2-txt0',
+                    '2-txt1',
+                ]
+            ],
+            'p': 2 / 3
         }
         parameters = json.loads(json.dumps(parameters))
         study = Study.Study(parameters)
         self.assertTrue(isinstance(study, Study.Study))
+
+    def test_tree(self):
+        parameters = {
+            'lvls': [
+                [
+                    '0-txt0'
+                ],
+                [
+                    '1-txt0',
+                    '1-txt1',
+                ],
+                [
+                    '2-txt0',
+                    '2-txt1',
+                ]
+            ],
+            'p': 2 / 3
+        }
+
+        parameters = json.loads(json.dumps(parameters))
+        study = Study.Study(parameters)
+        study.root.print()
+        # TODO: actually assert
