94adea4f1e69f3ffb10728957d45f1252004958f
Hyun-Joon Yang <yanghyun@stanford.edu>
2022-09-06 17:05:05 -0700

    implement setUp function for testing

--- app/test_Study.py
+++ app/test_Study.py
@@ -5,7 +5,8 @@ import Study
 
 
 class TestStudy(unittest.TestCase):
-    def test_instantiation(self):
+
+    def setUp(self) -> None:
         parameters = {
             'lvls': [
                 [
@@ -22,29 +23,21 @@ class TestStudy(unittest.TestCase):
             ],
             'p': 2 / 3
         }
-        parameters = json.loads(json.dumps(parameters))
-        study = Study.Study(parameters)
+        self.parameters = json.loads(json.dumps(parameters))
+
+    def test_instantiation(self):
+        study = Study.Study(self.parameters)
         self.assertTrue(isinstance(study, Study.Study))
 
     def test_tree(self):
-        parameters = {
-            'lvls': [
-                [
-                    '0-txt0'
-                ],
-                [
-                    '1-txt0',
-                    '1-txt1',
-                ],
-                [
-                    '2-txt0',
-                    '2-txt1',
-                ]
-            ],
-            'p': 2 / 3
-        }
-
-        parameters = json.loads(json.dumps(parameters))
-        study = Study.Study(parameters)
+        study = Study.Study(self.parameters)
         study.root.print()
         # TODO: actually assert
+
+    def test_initialCount(self):
+        study = Study.Study(self.parameters)
+        self.assertEqual(0, study.get_count([1, 1]))
+
+    def test_count(self):
+        study = Study.Study(self.parameters)
+        # TODO: add to count
