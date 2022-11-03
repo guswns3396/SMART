8667b8106633983c0c1b42bbd8abe0c4723e2a7b
Hyun-Joon Yang <yanghyun@stanford.edu>
2022-11-02 17:12:51 -0700

    added to todo list

--- flaskr/server.py
+++ flaskr/server.py
@@ -10,12 +10,13 @@ bp = Blueprint('server', __name__)
 
 # TODO: store answers
 # TODO: input validation [DONE until validating custom questions]
-# TODO: how to keep from leaving survey
+# TODO: how to keep from leaving survey (store subject's progress / config in db)
 # TODO: incorporate subject ID? => keep from retaking survey (integration REDCap?)
 # TODO: ask your own question
 # TODO: only 1 primary question?
 # TODO: server-side validation
 # TODO: keep from breaking (invalid study id, etc)
+# TODO: show amazon code after survey
 
 
 # @bp.errorhandler(KeyError)
